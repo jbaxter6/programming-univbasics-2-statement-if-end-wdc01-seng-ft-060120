72cdfbcd25c91d4b36c9101461b7c76f014b6c70
adept-logger-6002 <jbaxter6@radford.edu>
2020-04-08 05:11:18 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,6 +1,6 @@
 # Write your solution here
-current_time = Time.now;
-current_time = current.time.to_i
+current_time = Time.now
+current_time = current_time.to_i
 
 if current_time.even?
   put "Even!"
